cc14d726818d74c6e867f47a81433bd762317531
YashT02 <yashthakre839@gmail.com>
2021-12-14 02:12:30 +0530

    day4

--- day4/day4.py
+++ day4/day4.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 with open('day4/input.txt', 'r') as f:
     k = f.read().split('\n')
 
@@ -23,3 +25,42 @@ for i in range(len(bingo_grids)):
 for i in range(len(bingo_grids)):
     for k in range(5):
         bingo_grids[i][k] = [int(i) for i in bingo_grids[i][k]]
+
+num_called = list()
+
+bingo_grids = np.array(bingo_grids)
+
+flag = False
+
+for num in num_list:
+    for i in range(len(bingo_grids)):
+        for j in range(5):
+            if sum([test in num_called for test in bingo_grids[i][j]]) == 5:
+                answer = i
+                print('board found!!')
+                flag = True
+                break
+            if sum([test in num_called for test in bingo_grids[i, :, j]]) == 5:
+                answer = i
+                print('board found!!')
+                flag = True
+                break
+        if flag:
+            break
+    if flag:
+        break
+
+    num_called.append(num)
+
+
+winner_board = bingo_grids[answer]
+
+print(winner_board)
+
+score = 0
+
+for i in np.ravel(winner_board):
+    if i not in num_called:
+        score += i
+
+print(score * num_called[-1])
